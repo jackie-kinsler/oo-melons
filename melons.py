ce996f420d0bb09e98d5d9ca1a085bc208d498ce
Jackie Kinsler <jackiekinsler@gmail.com>
2020-10-05 20:50:38 +0000

    Initial commit.

---
+++ melons.py
@@ -0,0 +1,45 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder:
+    """An abstract class for other order classes to inherit from."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.country_code = country_code
+        self.shipped = False
+        self.order_type = "international"
+        self.tax = 0.17
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty, 'USA')
+
+
+
+class InternationalMelonOrder(AbstractMelonOrder:
+    """An international (non-US) melon order."""
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
